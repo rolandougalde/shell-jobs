3b1a17b81cc18f626f9b623b4652cb4e47a49088
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2023-07-21 11:18:23 -0600

    Update backup-fs.bat

--- dos-batch/backup-fs.bat
+++ dos-batch/backup-fs.bat
@@ -8,10 +8,10 @@ echo Archives accounting files and move them to the NAS.
 
 echo This script is intended to use with 7z v23.01..
 
-ECHO Creates a ZIP archive, named with current date and time.
+ECHO Creates a ZIP archive, named with current date and time. Using any type of compession will drain the hardware resources.
 "C:\Program Files\7-zip\7z.exe" a -tzip -ssw D:\Backup\[fs-name]-files-%date:~-4,4%-%date:~-10,2%-%date:~-7,2%-%time:~0,2%-%time:~3,2% D:\[fs-name]\ >[log-name]-%date:~-4,4%-%date:~-10,2%-%date:~-7,2%-%time:~0,2%-%time:~3,2%.txt 2>&1
 
-ECHO Creates a TAR archive, named with current date and time.
+ECHO Creates a TAR archive, named with current date and time. Using TAR will reduce the CPU inpact.
 "C:\Program Files\7-zip\7z.exe" a -ttar -ssw D:\Backup\[fs-name]-files-%date:~-4,4%-%date:~-10,2%-%date:~-7,2%-%time:~0,2%-%time:~3,2% D:\[fs-name]\ >[log-name]-%date:~-4,4%-%date:~-10,2%-%date:~-7,2%-%time:~0,2%-%time:~3,2%.txt 2>&1
 
 echo Removing work files...
