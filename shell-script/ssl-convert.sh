a7023e6ef5cbd7bc23d44d7a25c59496de34bd57
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2023-07-22 16:58:21 -0600

    Update ssl-convert.sh

--- shell-script/ssl-convert.sh
+++ shell-script/ssl-convert.sh
@@ -9,9 +9,9 @@ unzip *.zip
 # Set variables
 
  # Concatante on a single file
-cat ba256bfc8f0bcfec.crt gd_bundle-g2-g1.crt betheritage.eu.key > 2024.wildcard.betheritage.eu.crt
+cat ba256bfc8f0bcfec.crt gd_bundle-g2-g1.crt [domain-name].eu.key > 2024.wildcard.[domain-name].eu.crt
 
 # Convert certificate to PKCS12/PFX
-openssl pkcs12 -export -out 2024.wildcard.betheritage.eu.pfx -inkey betheritage.eu.key -in ba256bfc8f0bcfec.crt # -certfile bundle.[domain-name].crt
+openssl pkcs12 -export -out 2024.wildcard.[domain-name].eu.pfx -inkey [domain-name].eu.key -in ba256bfc8f0bcfec.crt # -certfile bundle.[domain-name].crt
 
 exit 0
