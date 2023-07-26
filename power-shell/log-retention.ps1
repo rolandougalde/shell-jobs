6df1a3d29ba9d59adea39e513121f2b358b8f359
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2023-07-26 15:11:31 -0600

    Create log-retention.ps1

---
+++ power-shell/log-retention.ps1
@@ -0,0 +1,25 @@
+# This script was tested deleting web server multiple files
+# Set the root directory where the search will begin
+$rootDirectory = "C:\inetpub\logs"
+
+# Function to recursively delete *.log files in subdirectories
+function DeleteLogFilesRecursively($directory) {
+    # Get all *.log files in the current directory
+    $logFiles = Get-ChildItem -Path $directory -Filter "*.log" -File
+
+    # Delete each *.log file found
+    foreach ($logFile in $logFiles) {
+        $logFilePath = $logFile.FullName
+        Remove-Item $logFilePath -Force
+        Write-Host "Deleted: $logFilePath"
+    }
+
+    # Recursively search and delete in subdirectories
+    $subDirectories = Get-ChildItem -Path $directory -Directory
+    foreach ($subDir in $subDirectories) {
+        DeleteLogFilesRecursively $subDir.FullName
+    }
+}
+
+# Start the recursive search and deletion process
+DeleteLogFilesRecursively $rootDirectory
