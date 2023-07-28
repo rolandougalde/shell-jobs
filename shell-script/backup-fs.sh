a1cf205d6ae5c3415da020ee22f1e313558fed28
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2023-07-28 14:02:40 -0600

    Update backup-fs.sh

--- shell-script/backup-fs.sh
+++ shell-script/backup-fs.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+# This script requires Cron in order to run the sheduled tasks.
+# Type "crontab -e" in order to setup Cron.
+
 # Create websites backup starting at at 1:00 a.m.
 0 1 * * * /bin/tar -zcf /home/[user-directory]/backups/[backup-name]-sites-$(date +\%Y_\%m_\%d_\%H_\%M).tar.gz /var/www
+
+exit 0
