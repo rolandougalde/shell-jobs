55b9b2d3f2d55bfd8fb33665b481bc50f3f9f2a6
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2023-07-29 14:54:43 -0600

    Update debian-12-build.sh

--- shell-script/debian-12-build.sh
+++ shell-script/debian-12-build.sh
@@ -6,6 +6,7 @@
 
 # Post install packages installation
 apt-get install sudo
+apt-get install htop
 apt-get install linux-image-cloud-amd64
 apt-get autoremove linux-image-amd64*
 apt-get autoremove linux-firmware
@@ -15,5 +16,18 @@ apt-get autoremove intel-microcode
 apt-get install apache2
 
 # PHP legacy version installation
+# Install software-properties-common, which adds management for additional software sources:
+sudo apt -y install software-properties-common
+
+# Install the repository ppa:ondrej/php, which will give you all your versions of PHP:
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get update
+
+# Install the desired version of PHP
+sudo apt -y install php7.4
+
+# Install the desired version of PHP-FPM
+sudo apt -y install php7.4-fpm
 
 exit 0
