0bce1e414cfae637050811041621759e749d0040
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2023-09-15 13:25:25 -0600

    With hyphens

--- shell-script/backup-fs.sh
+++ shell-script/backup-fs.sh
@@ -4,6 +4,6 @@
 # Type "crontab -e" in order to setup Cron.
 
 # Create websites backup starting at at 1:00 a.m.
-0 1 * * * /bin/tar -zcf /home/[user-directory]/backups/[backup-name]-sites-$(date +\%Y_\%m_\%d_\%H_\%M).tar.gz /var/www
+0 1 * * * /bin/tar -zcf /home/[user-directory]/backups/[backup-name]-sites-$(date +\%Y-\%m-\%d-\%H-\%M).tar.gz /var/www
 
 exit 0
