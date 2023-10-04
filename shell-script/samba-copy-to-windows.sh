02cc303104318a29c6e105854e2e03b374fb3197
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2023-10-04 17:43:56 -0600

    Update samba-copy-to-windows.sh

--- shell-script/samba-copy-to-windows.sh
+++ shell-script/samba-copy-to-windows.sh
@@ -7,3 +7,7 @@ smbclient '//destination/folder' -U "DOMAINNAME\username" -c "put backup_2023100
 smbclient '//xxx.xxx.xxx.xxx/tmp' -U Administrator -c "put /home/user/notes.txt home-folder-user" -m SMB3
 
 smbclient '\\server\share' -N -c 'prompt OFF;recurse ON;cd 'path\to\directory\';lcd '~/path/to/download/to/';mget *'`
+
+# Better version
+smbclient '\\server\share' -N -c 'prompt OFF;recurse ON;cd "path\to\directory\";lcd "~/path/to/download/to/";mget *'
+
