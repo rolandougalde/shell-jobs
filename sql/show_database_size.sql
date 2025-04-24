b0fa7e49a78832300a18058bf39f8a0c0f302efb
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2025-04-24 16:35:25 -0600

    Update show_database_size.sql

--- sql/show_database_size.sql
+++ sql/show_database_size.sql
@@ -1,4 +1,17 @@
-SELECT table_schema "DB Name",
-        ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" 
-FROM information_schema.tables 
-GROUP BY table_schema; 
+-- display a database size
+
+SELECT table_schema AS "Database",
+       ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS "Size (MB)"
+FROM information_schema.tables
+WHERE table_schema = 'osticket'
+GROUP BY table_schema;
+
+
+-- display alk databases size
+
+SELECT table_schema AS "Database",
+       ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS "Size (MB)"
+FROM information_schema.tables
+GROUP BY table_schema
+ORDER BY `Size (MB)` DESC;
+
