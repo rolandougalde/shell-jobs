d3f12e3e081e6c09d697b568d64298f82ea410e9
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2026-08-19 15:46:17 -0600

    Update permissions-script.sh

--- shell-script/permissions-script.sh
+++ shell-script/permissions-script.sh
@@ -1,5 +1,7 @@
+# CMS permisions Schema
+
 # Reset web folder (to open) permisions at 3 a.m.
-* 3 * * * /usr/bin/find /var/www/ -type d -exec chmod 775 {} + && sudo find /var/www/ -type f -exec chmod 664 {} +
+* 3 * * * /usr/bin/find /var/www/  find -type d -exec chmod 775 {} + && sudo find /var/www/ -type f -exec chmod 664 {} +
 
 # Reset web folder (to closer) permisions at 3 a.m.
-* 3 * * * /usr/bin/find /var/www/ -type d -exec chmod 755 {} + && sudo find /var/www/ -type f -exec chmod 644 {} +
+* 3 * * * /usr/bin/find /var/www/ find -type d -exec chmod 755 {} + && sudo find /var/www/ -type f -exec chmod 644 {} +
